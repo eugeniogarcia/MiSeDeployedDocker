039993f89ca4c3554cb9049821be977abaee9c8d
Eugenio Garcia San Martin <egsmartin@gmail.com>
2019-06-22 15:40:40 +0200

    Update versions

--- zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/TrackingFilter.java
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/TrackingFilter.java
@@ -1,59 +1,62 @@
 package com.thoughtmechanix.zuulsvr.filters;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
 @Component
 public class TrackingFilter extends ZuulFilter{
-    private static final int      FILTER_ORDER =  1;
-    private static final boolean  SHOULD_FILTER=true;
-    private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+	private static final int      FILTER_ORDER =  1;
+	private static final boolean  SHOULD_FILTER=true;
+	private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
 
-    @Autowired
-    FilterUtils filterUtils;
+	@Autowired
+	FilterUtils filterUtils;
 
-    @Override
-    public String filterType() {
-        return FilterUtils.PRE_FILTER_TYPE;
-    }
+	@Override
+	public String filterType() {
+		return FilterUtils.PRE_FILTER_TYPE;
+	}
 
-    @Override
-    public int filterOrder() {
-        return FILTER_ORDER;
-    }
+	@Override
+	public int filterOrder() {
+		return FILTER_ORDER;
+	}
 
-    public boolean shouldFilter() {
-        return SHOULD_FILTER;
-    }
+	@Override
+	public boolean shouldFilter() {
+		return SHOULD_FILTER;
+	}
 
-    private boolean isCorrelationIdPresent(){
-      if (filterUtils.getCorrelationId() !=null){
-          return true;
-      }
+	private boolean isCorrelationIdPresent(){
+		if (filterUtils.getCorrelationId() !=null){
+			return true;
+		}
 
-      return false;
-    }
+		return false;
+	}
 
-    private String generateCorrelationId(){
-        return java.util.UUID.randomUUID().toString();
-    }
+	private String generateCorrelationId(){
+		return java.util.UUID.randomUUID().toString();
+	}
 
-    public Object run() {
+	@Override
+	public Object run() {
 
-        if (isCorrelationIdPresent()) {
-            logger.debug("tmx-correlation-id found in tracking filter: %s. ", filterUtils.getCorrelationId());
-        }
-        else{
-            filterUtils.setCorrelationId(generateCorrelationId());
-            logger.debug("tmx-correlation-id generated in tracking filter: {}.", filterUtils.getCorrelationId());
-        }
+		if (isCorrelationIdPresent()) {
+			logger.debug("tmx-correlation-id found in tracking filter: %s. ", filterUtils.getCorrelationId());
+		}
+		else{
+			filterUtils.setCorrelationId(generateCorrelationId());
+			logger.debug("tmx-correlation-id generated in tracking filter: {}.", filterUtils.getCorrelationId());
+		}
 
-        RequestContext ctx = RequestContext.getCurrentContext();
-        logger.debug("Processing incoming request for {}.",  ctx.getRequest().getRequestURI());
-        return null;
-    }
+		final RequestContext ctx = RequestContext.getCurrentContext();
+		logger.debug("Processing incoming request for {}.",  ctx.getRequest().getRequestURI());
+		return null;
+	}
 }
